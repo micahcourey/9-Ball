754ed3ac8f2bce81f5d9ca7b6a1f065133189e02
micahcourey <micahjcourey@gmail.com>
2016-01-19 21:09:23 -0800

    Add scaffolding for game components

--- script.jsx
+++ script.jsx
@@ -1,6 +1,53 @@
-// Code goes here
+var StarsFrame = React.createClass({
+  render: function() {
+    return (
+      <div id="stars-frame">
+        <div className="well">
+          <span className="glyphicon glyphicon-star"></span>
+          <span className="glyphicon glyphicon-star"></span>
+          <span className="glyphicon glyphicon-star"></span>
+          <span className="glyphicon glyphicon-star"></span>
+        </div>
+      </div>
+    );
+  }
+});
+
+var ButtonFrame = React.createClass({
+  render: function() {
+    return (
+      <div>
+      ...
+      </div>
+    );
+  }
+});
+
+var AnswerFrame = React.createClass({
+  render: function() {
+    return (
+      <div>
+      ...
+      </div>
+    );
+  }
+});
+
+var Game = React.createClass({
+  render: function() {
+    return (
+      <div id="game">
+        <h2>9 Ball</h2>
+        <StarsFrame />
+        <ButtonFrame />
+        <AnswerFrame />
+
+      </div>
+    );
+  }
+});
 
 React.render(
-  <h1>Hello React!</h1>,
+  <Game />,
   document.getElementById('container')
 );
