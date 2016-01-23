ae6a8b40be7d7853955fcae2744ae06205f731fc
micahcourey <micahjcourey@gmail.com>
2016-01-23 13:31:20 -0800

    Add files to gh-pages

---
+++ README.md
@@ -0,0 +1,10 @@
+9-Ball
+======
+
+By Micah Courey, 18-Jan-2016
+
+A number game built in React.
+
+Technologies Used
+----------
+JavaScript, React, jQuery, Bootstrap, HTML, CSS
---
+++ script.jsx
@@ -0,0 +1,88 @@
+var StarsFrame = React.createClass({
+  render: function() {
+    var numberOfStars = Math.floor(Math.random()*9) + 1;
+
+    var stars = [];
+    for (var i=0; i<numberOfStars; i++) {
+      stars.push(
+        <span className="glyphicon glyphicon-star"></span>
+      );
+    }
+
+    return (
+      <div id="stars-frame">
+        <div className="well">
+          {stars}
+        </div>
+      </div>
+    );
+  }
+});
+
+var ButtonFrame = React.createClass({
+  render: function() {
+    return (
+      <div id="button-frame">
+       <button className="btn btn-primary">=</button>
+      </div>
+    );
+  }
+});
+
+var AnswerFrame = React.createClass({
+  render: function() {
+    return (
+      <div id="answer-frame">
+        <div className="well">
+          {this.props.selectedNumbers}
+        </div>
+      </div>
+    );
+  }
+});
+
+var NumbersFrame = React.createClass({
+  render: function() {
+
+    var numbers = [], className, selectedNumbers = this.props.selectedNumbers;
+    for (var i=1; i <= 9; i++) {
+      className = "number selected-" + (selectedNumbers.indexOf(i)>=0);
+      numbers.push(
+        <div className={className}>{i}</div>
+      );
+    }
+    return (
+      <div id="numbers-frame">
+        <div className="well">
+          {numbers}
+        </div>
+      </div>
+    );
+  }
+});
+
+var Game = React.createClass({
+  getInitialState: function() {
+    return {selectedNumbers: [3, 6]};
+  },
+  render: function() {
+    return (
+      <div id="game">
+        <h2>9 Ball</h2>
+        <div className="clearfix">
+          <StarsFrame />
+          <ButtonFrame />
+          <AnswerFrame selectedNumbers = {this.state.selectedNumbers} />
+        </div>
+
+        <NumbersFrame selectedNumbers = {this.state.selectedNumbers} />
+
+      </div>
+    );
+  }
+});
+
+React.render(
+  <Game />,
+  document.getElementById('container')
+);
