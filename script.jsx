52868c566867df4c490d44a74a1cf9d2b67df9e5
micahcourey <micahjcourey@gmail.com>
2016-01-28 19:37:58 -0800

    Add ability to check if an answer is right or wrong and update the button accordingly

--- script.jsx
+++ script.jsx
@@ -19,13 +19,36 @@ var StarsFrame = React.createClass({
 
 var ButtonFrame = React.createClass({
   render: function() {
-    var disabled;
-    disabled = (this.props.selectedNumbers.length === 0);
+    var disabled, button, correct = this.props.correct;
+
+    switch(correct) {
+      case true:
+         button = (
+          <button className="btn btn-success btn-lg">
+            <span className="glyphicon glyphicon-ok"></span>
+          </button>
+        );
+        break;
+      case false:
+         button = (
+          <button className="btn btn-danger btn-lg">
+            <span className="glyphicon glyphicon-remove"></span>
+          </button>
+        );
+        break;
+      default:
+        disabled = (this.props.selectedNumbers.length === 0);
+        button = (
+          <button className="btn btn-primary btn-lg" disabled={disabled}
+                  onClick={this.props.checkAnswer} >
+            =
+          </button>
+        );
+    }
+
     return (
       <div id="button-frame">
-       <button className="btn btn-primary" disabled={disabled}>
-        =
-       </button>
+        {button}
       </div>
     );
   }
@@ -80,7 +103,9 @@ var NumbersFrame = React.createClass({
 var Game = React.createClass({
   getInitialState: function() {
     return {numberOfStars: Math.floor(Math.random()*9) + 1,
-            selectedNumbers: []};
+            selectedNumbers: [],
+            correct: null
+    };
   },
   selectNumber: function(clickedNumber) {
     if (this.state.selectedNumbers.indexOf(clickedNumber) < 0) {
@@ -96,15 +121,27 @@ var Game = React.createClass({
     selectedNumbers.splice(indexOfNumber, 1);
     this.setState({selectedNumbers: selectedNumbers});
   },
+  sumOfSelectedNumbers: function() {
+    return this.state.selectedNumbers.reduce(function(p,n) {
+      return p+n;
+    }, 0)
+  },
+  checkAnswer: function() {
+    var correct = (this.state.numberOfStars === this.sumOfSelectedNumbers());
+    this.setState({correct: correct});
+  },
   render: function() {
     var selectedNumbers = this.state.selectedNumbers,
-          numberOfStars = this.state.numberOfStars;
+        numberOfStars = this.state.numberOfStars;
+        correct = this.state.correct;
     return (
       <div id="game">
         <h2>9 Ball</h2>
         <div className="clearfix">
           <StarsFrame numberOfStars = {numberOfStars} />
-          <ButtonFrame selectedNumbers = {selectedNumbers} />
+          <ButtonFrame selectedNumbers = {selectedNumbers}
+                               correct = {correct}
+                               checkAnswer={this.checkAnswer} />
           <AnswerFrame selectedNumbers = {selectedNumbers}
                         unselectNumber = {this.unselectNumber} />
         </div>
